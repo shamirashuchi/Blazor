1774d104322ccd1db803614fe8bac4a5e2932b75
Shamira Shuchi <100753311+shamirashuchi@users.noreply.github.com>
2025-02-23 08:30:44 +0600

    first commit

---
+++ EmployeeManagement.web/Components/Pages/EmployeeListBase.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using EmployeeManagement.Models;
+namespace EmployeeManagement.web.Components.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+}
